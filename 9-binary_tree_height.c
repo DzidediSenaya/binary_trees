baf2b3c281034e581f4032e5b09d53a3b38df79a
DzidediSenaya <dzidedisenaya@gmail.com>
2023-08-31 10:02:15 -0700

    betty style fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,19 +9,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree) /*If tree is not NULL*/
-    {
-        size_t l = 0, r = 0; /*Declare variables to store left and right heights*/
+if (tree) /*If tree is not NULL*/
+{
+size_t l = 0, r = 0; /*Declare variables to store left and right heights*/
 
-        /*Calculate the height of the left subtree*/
-        l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        
-        /* Calculate the height of the right subtree*/
-        r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        
-        /*Return the maximum of the two heights plus 1 (to count the current node)*/
-        return ((l > r) ? l : r);
-    }
-    
-    return (0); /*If tree is NULL, return 0*/
+/*Calculate the height of the left subtree*/
+l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+
+/* Calculate the height of the right subtree*/
+r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+/*Return the maximum of the two heights plus 1 (to count the current node)*/
+return ((l > r) ? l : r);
+}
+return (0); /*If tree is NULL, return 0*/
 }
